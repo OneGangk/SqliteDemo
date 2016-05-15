638a1d5c5d1f4d46edf05c010049b910eca0f2bc
likangding <likangding@likangding.local>
2016-05-15 22:21:41 +0800

    Initial Commit

---
+++ SqliteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteDemo
+//
+//  Created by likangding on 16/5/15.
+//  Copyright © 2016年 likangding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SqliteDemo
+//
+//  Created by likangding on 16/5/15.
+//  Copyright © 2016年 likangding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
